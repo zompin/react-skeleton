3d278740aa9a88b1d1d3820ead926e198570f188
Ilya Altukhov <zompin@ya.ru>
2018-09-07 23:57:11 +0300

    Init

---
+++ src/js/containers/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+
+class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    );
+  }
+}
+
+export default hot(module)(App);
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = (_, { mode }) => {
+  const isDevelopment = mode === 'development';
+
+  return {
+    entry: './src/js/index.jsx',
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      publicPath: isDevelopment ? '/' : '/dist/',
+    },
+
+    resolve: {
+      extensions: ['.jsx', '.js', 'less'],
+    },
+
+    module: {
+      rules: [{
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+      }, {
+        test: /\.less$/,
+        loaders: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [autoprefixer],
+            }
+          },
+          'less-loader',
+        ],
+      }],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        hash: true,
+      }),
+      new HotModuleReplacementPlugin(),
+      new MiniCssExtractPlugin(),
+    ],
+
+    devServer: {
+      contentBase: isDevelopment ? __dirname : path.join(__dirname, 'dist'),
+      port: 3000,
+      hot: true,
+      open: true,
+    },
+  };
+};
