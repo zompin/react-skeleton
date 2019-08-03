778f858b066b09d2b3684c7c7c08ebc905ef08ac
Ilya Altukhov <zompin@ya.ru>
2019-08-03 18:54:40 +0300

    Eslint fix

--- src/js/utils/history.js
+++ src/js/utils/history.js
@@ -1,5 +1,6 @@
 import { createBrowserHistory, createHashHistory } from 'history';
 
+// eslint-disable-next-line
 const history = NODE_ENV === 'development' ? createHashHistory() : createBrowserHistory();
 
 export default history;
